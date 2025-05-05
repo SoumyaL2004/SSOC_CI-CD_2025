d2eb522b93d45cc72fc957124c3d584b52f3ccb0
SoumyaN8 <soumyanidasosi6@gmail.com>
2025-05-05 21:58:50 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
  #include <iostream>
 
  int main() {
-     std::cout << "Hello, SystemC CI/CD World!" << std::endl;
+     std::cout << " Hello, SystemC CI/CD World! " << std::endl;
      return 0;
  }
