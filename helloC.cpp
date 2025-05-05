136c53fbf9ab8cfd20d4667352d5cbb4df363261
SoumyaN8 <soumyanidasosi6@gmail.com>
2025-05-05 22:24:22 +0530

    Update helloC.cpp

--- helloC.cpp
+++ helloC.cpp
@@ -1,22 +1,17 @@
 #include <systemc.h>
 
-SC_MODULE(HelloWorld) {
-    SC_CTOR(HelloWorld) {
-        // Simple process to print "Hello, SystemC CI/CD World!" at time 0
-        SC_THREAD(print_message);
+SC_MODULE(HelloSystemC) {
+    SC_CTOR(HelloSystemC) {
+        SC_THREAD(print_hello);
     }
 
-    void print_message() {
+    void print_hello() {
         std::cout << "Hello, SystemC CI/CD World!" << std::endl;
     }
 };
 
 int sc_main(int argc, char* argv[]) {
-    // Instantiate the HelloWorld module
-    HelloWorld hello("HelloWorld");
-
-    // Start the simulation
+    HelloSystemC hello("hello");
     sc_start();
-
     return 0;
 }
